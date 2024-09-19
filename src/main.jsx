c03797c6880f7bca776d106c94829825e8c866d4
Kazeem <58448835+kazeems@users.noreply.github.com>
2024-09-19 15:24:44 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 // eslint-disable-next-line react-refresh/only-export-components
-Const Data = [  
-  { Id: "Todo-0", Name: "Eat", Completed: True },
-  { Id: "Todo-1", Name: "Sleep", Completed: False },
-  { Id: "Todo-2", Name: "Repeat", Completed: False },
-];
+Const Data = [];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App tasks={Data} />
   </React.StrictMode>,
 )
